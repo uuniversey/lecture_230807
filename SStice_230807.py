997511508a1308a98b53d6384614242311522693
unknown <mm9326@naver.com>
2023-08-07 22:42:46 +0900

    house commit

--- SStice_230807.py
+++ SStice_230807.py
@@ -8,12 +8,37 @@
 
 # str 함수를 사용하지 않고 itoa(a)를 구현해본다.
 
-def itoa(a):
-    s = ''
-    while a>0:
-        s += chr(ord('0') + a % 10)   #1의 자리 숫자의 ASCII 값
-        a //= 10
-    return s[::-1]
+# def itoa(a):
+#     s = ''
+#     while a>0:
+#         s += chr(ord('0') + a % 10)   #1의 자리 숫자의 ASCII 값
+#         a //= 10
+#     return s[::-1]
+#
+# a = 123
+# print(itoa(a))
 
-a = 123
-print(itoa(a))
+
+
+# 직사각형 네개의 합집합의 면적 구하기
+# 각 상황 마다의 넓이를 구하고 겹치는 장소는 몇번 겹치는지 확인한 후 겹치는 숫자 만큼 빼줌
+
+# box = [[0] * 100 for _ in range(100)]                   # 최대 범위의 빈 박스 생성
+# area = 0                                                # 넓이 초기값
+# for i in range(4):                                      # 인풋을 4번 나눠 받음
+#     a, b, c, d = map(int,input().split())
+#     area += (c-a) * (d-b)                               # 각 상황 마다의 넓이
+#     for row in range(a, c):                             # 인풋 받은 정수를 바탕으로 박스마다 1을 채움
+#         for col in range(b, d):
+#             box[row][col] += 1
+#
+# overlab = 0                                             # 겹치는 곳의 초기값
+#
+# for row in range(100):
+#     for col in range(100):
+#         if box[row][col] >= 2:                          # 겹쳐 있다는 것은 박스에 숫자가 2이상이라는 뜻
+#             overlab += box[row][col] - 1                # 겹쳐 있는 숫자만큼 빼줌(1이 남을 때까지 )
+#
+# result = area - overlab                                 # 전체 넓이의 합에 겹쳐있는 만큼 빼면 답
+#
+# print(result)
--- daily_230807.py
+++ daily_230807.py
@@ -2,63 +2,14 @@
 
 # 1221. 5일차 - GNS
 
-# import sys
-# sys.stdin = open('input_1221.txt', 'r')
-#
-# T = int(input())
-# for tc in range(1, T+1):
-#     N = list(input().split(' '))
-#     my_list = list(input().split(' '))
-#
-#     for i in range(len(my_list)):
-#         if my_list[i] == 'ZRO':
-#             my_list[i] = 0
-#         elif my_list[i] == 'ONE':
-#             my_list[i] = 1
-#         elif my_list[i] == 'TWO':
-#             my_list[i] = 2
-#         elif my_list[i] == 'THR':
-#             my_list[i] = 3
-#         elif my_list[i] == 'FOR':
-#             my_list[i] = 4
-#         elif my_list[i] == 'FIV':
-#             my_list[i] = 5
-#         elif my_list[i] == 'SIX':
-#             my_list[i] = 6
-#         elif my_list[i] == 'SVN':
-#             my_list[i] = 7
-#         elif my_list[i] == 'EGT':
-#             my_list[i] = 8
-#         elif my_list[i] == 'NIN':
-#             my_list[i] = 9
-#
-#     my_list.pop()                               # 맨 마지막에 '' 제거
-#     my_list.sort()                              # 리스트 정렬
-#
-#     for i in range(len(my_list)):
-#         if my_list[i] == 0:
-#             my_list[i] = 'ZRO'
-#         elif my_list[i] == 1:
-#             my_list[i] = 'ONE'
-#         elif my_list[i] == 2:
-#             my_list[i] = 'TWO'
-#         elif my_list[i] == 3:
-#             my_list[i] = 'THR'
-#         elif my_list[i] == 4:
-#             my_list[i] = 'FOR'
-#         elif my_list[i] == 5:
-#             my_list[i] = 'FIV'
-#         elif my_list[i] == 6:
-#             my_list[i] = 'SIX'
-#         elif my_list[i] == 7:
-#             my_list[i] = 'SVN'
-#         elif my_list[i] == 8:
-#             my_list[i] = 'EGT'
-#         elif my_list[i] == 9:
-#             my_list[i] = 'NIN'
-#
-#     print(f'#{tc}')
-#     print(*my_list)
+import sys
+sys.stdin = open('input_1221.txt', 'r')
+
+T = int(input())
+for tc in range(1, T+1):
+    N = list(input().split(' '))
+    my_list = list(input().split(' '))
+
 
 
 
@@ -89,28 +40,37 @@
 
 # 18222. 회문
 
-import sys
-sys.stdin = open('input_18222.txt','r')
+# import sys
+# sys.stdin = open('input_18222.txt','r')
+#
+# T = int(input())
+# for tc in range(1, T+1):
+#     N, M = map(int,input().split())
+#     arr = [list(map(str,input())) for _ in range(N)]
+#     my_str = ''
+#     result = []
+#
+#     for row in range(N):                        # 가로 파리채
+#         for col in range(N):
+#             my_str = ''
+#             for row2 in range(M):
+#                 if 0 <= row + row2 < N:
+#                     my_str += arr[row + row2][col]
+#                     if len(my_str) == M and my_str[::-1] == my_str:
+#                         result.append(my_str)
+#
+#     for col in range(N):                        # 세로 파리채
+#         for row in range(N):
+#             my_str = ''
+#             for col2 in range(M):
+#                 if 0 <= col + col2 < N:
+#                     my_str += arr[row][col + col2]
+#                     if len(my_str) == M and my_str[::-1] == my_str:
+#                         result.append(my_str)
+#
+#     print(f'#{tc}', *result)
 
-T = int(input())
-for tc in range(1, T+1):
-    N, M = map(int,input().split())
-    arr = [list(map(str,input())) for _ in range(N)]
-    my_str = ''
-    for row in range(N):                        # 가로 파리채
-        my_str = ''
-        for col in range(N):
-            for row2 in range(M):
-                if 0 <= row + row2 < N
-                    my_str += arr[row + row2][col + col2]
 
-            if my_str[::-1] ==
-
-    print(my_str)
-
-
-        # 1. m 길이로 다 빼놓고
-        # 2. 회문인거 찾기
 
 
 
